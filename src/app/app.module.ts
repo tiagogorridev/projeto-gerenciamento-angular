814404449a30c7f5a166d3adb59092faf41660fc
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-06 02:13:51 -0300

    feat: criacao do componente profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { ReportsComponent } from './reports/reports.component';
 import { TimesheetComponent } from './timesheet/timesheet.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { TimesheetComponent } from './timesheet/timesheet.component';
     ActivitiesComponent,
     ReportsComponent,
     TimesheetComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
