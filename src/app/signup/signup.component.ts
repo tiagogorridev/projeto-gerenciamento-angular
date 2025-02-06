c99eae71e5242b0e659d050589af90e422bf5fba
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-06 11:55:44 -0300

    feature: implementacao da tela signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  fullName: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      console.error('As senhas não coincidem!');
+      return;
+    }
+
+    console.log('Nome Completo:', this.fullName);
+    console.log('Email:', this.email);
+    console.log('Senha:', this.password);
+
+
+    this.router.navigate(['/login']);
+  }
 }
