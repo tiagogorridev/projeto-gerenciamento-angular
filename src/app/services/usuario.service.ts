888442826f6fef650a8329d6a4c0d233d66cabc4
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-07 15:19:06 -0300

    feature: criacao do usuario.service.ts juntamente com implementacao para conexao com api

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from './usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private apiUrl = 'http://localhost:8080/api/usuarios';
+
+  constructor(private http: HttpClient) {}
+
+  cadastrarUsuario(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.apiUrl}/cadastro`, usuario);
+  }
+}
