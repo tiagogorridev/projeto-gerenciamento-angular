efb5014957fc05d297a91395247a8bfd1cd27ff8
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-11 14:37:26 -0300

    chore: correção de formatação no AuthGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,6 +4,7 @@ import { CanActivate, Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
+export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
