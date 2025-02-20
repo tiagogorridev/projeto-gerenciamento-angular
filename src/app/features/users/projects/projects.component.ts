84ef1186f30e2e831de7254becf408462445d2e7
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-20 15:28:36 -0300

    feature: implementa o método 'ngOnInit' para buscar os projetos associados com base no 'usuario_id' do localStorage

--- src/app/features/users/projects/projects.component.ts
+++ src/app/features/users/projects/projects.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../../../core/auth/services/projects.service'; // Caminho do seu serviço
+import { Projeto } from '../../../core/auth/services/projeto.model'; // Modelo de Projeto, ajuste conforme sua estrutura
 
 @Component({
-  selector: 'app-projects',
+  selector: 'app-projetos-usuario',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
 
+  projetos: Projeto[] = []; // Declarando a propriedade projetos
+
+  constructor(private projetoService: ProjectsService) { }
+
+  ngOnInit(): void {
+    const usuarioId = localStorage.getItem('usuario_id'); // ou use um serviço de autenticação
+    if (usuarioId) {
+      this.projetoService.getProjetosAssociados(+usuarioId).subscribe(projetos => {
+        console.log(projetos);
+        this.projetos = projetos;
+      });
+    }
+  }
 }
