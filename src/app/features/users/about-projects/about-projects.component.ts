24ec0f30644e3cebb99bf359228dcfd31839e553
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-21 09:32:04 -0300

    style: estilizacao dos projetos para usuarios

--- src/app/features/users/about-projects/about-projects.component.ts
+++ src/app/features/users/about-projects/about-projects.component.ts
@@ -11,9 +11,18 @@ import { Tarefa } from '../../../core/auth/services/tarefa.model';
   styleUrls: ['./about-projects.component.scss']
 })
 export class AboutProjectsComponent implements OnInit {
-
   projeto!: Projeto;
   tarefas: Tarefa[] = [];
+  showModal: boolean = false;
+
+  timeTracking = {
+    startDate: new Date(),
+    startTime: '',
+    endTime: '',
+    duration: '',
+    selectedTask: '',
+    description: ''
+  };
 
   constructor(
     private route: ActivatedRoute,
@@ -33,5 +42,58 @@ export class AboutProjectsComponent implements OnInit {
         });
       });
     }
+  }
+
+  openAddHoursModal(): void {
+    this.showModal = true;
+    this.timeTracking.startDate = new Date();
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+    this.resetTimeTracking();
+  }
+
+  resetTimeTracking(): void {
+    this.timeTracking = {
+      startDate: new Date(),
+      startTime: '',
+      endTime: '',
+      duration: '',
+      selectedTask: '',
+      description: ''
+    };
+  }
+
+  calculateDuration(): void {
+    if (this.timeTracking.startTime && this.timeTracking.endTime) {
+      const start = this.parseTime(this.timeTracking.startTime);
+      const end = this.parseTime(this.timeTracking.endTime);
+
+      if (start !== undefined && end !== undefined) {
+        const diff = end - start;
+        this.timeTracking.duration = this.formatDuration(diff);
+      }
+    }
+  }
+
+  parseTime(time: string): number | undefined {
+    const [hours, minutes] = time.split(':').map(num => parseInt(num, 10));
+    if (isNaN(hours) || isNaN(minutes)) {
+      return undefined;
+    }
+    return hours * 60 + minutes;
+  }
+
+  formatDuration(minutes: number): string {
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hours}h ${mins}m`;
+  }
+
+  submitHours(): void {
+    // Implement your hours submission logic here
+    console.log('Submitting hours:', this.timeTracking);
+    this.closeModal();
   }
 }
