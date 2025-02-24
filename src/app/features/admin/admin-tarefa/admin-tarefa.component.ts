3607f9d61b406822897f015dd34a9534a4c19e9c
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-24 11:51:19 -0300

    fix: ajustado método de carregamento de tarefa

--- src/app/features/admin/admin-tarefa/admin-tarefa.component.ts
+++ src/app/features/admin/admin-tarefa/admin-tarefa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TarefaService } from '../../../core/auth/services/tarefa.service';
 
 @Component({
   selector: 'app-admin-tarefa',
@@ -8,13 +9,50 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AdminTarefaComponent implements OnInit {
   idprojeto: number = 0;
-  idtarefa: number = 0;  // Altere para idtarefa em vez de id
+  idtarefa: number = 0;
+  tarefa: any = {};
+  isEditMode: boolean = false;
+  statusOpcoes: string[] = ['ABERTA', 'EM_ANDAMENTO', 'CONCLUIDA', 'PAUSADA'];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private tarefaService: TarefaService
+  ) {}
 
   ngOnInit(): void {
-    // Captura os parâmetros da rota
     this.idprojeto = +this.route.snapshot.paramMap.get('idprojeto')!;
-    this.idtarefa = +this.route.snapshot.paramMap.get('idtarefa')!;  // Atualize para 'idtarefa'
+    this.idtarefa = +this.route.snapshot.paramMap.get('idtarefa')!;
+
+    console.log('idprojeto:', this.idprojeto);
+    console.log('idtarefa:', this.idtarefa);
+
+    this.tarefaService.getTarefaDetails(this.idprojeto, this.idtarefa)
+      .subscribe(
+        (data) => {
+          this.tarefa = data;
+          console.log('Tarefa carregada:', this.tarefa);
+        },
+        (error) => {
+          console.error('Erro ao carregar a tarefa:', error);
+        }
+      );
+  }
+
+  editarTarefa(): void {
+    this.isEditMode = true;
+  }
+
+  salvarAlteracoes(): void {
+    this.tarefaService.atualizarTarefa(this.idprojeto, this.idtarefa, this.tarefa)
+      .subscribe(
+        (data) => {
+          this.tarefa = data;
+          console.log('Tarefa atualizada:', this.tarefa);
+          this.isEditMode = false;
+        },
+        (error) => {
+          console.error('Erro ao atualizar a tarefa:', error);
+        }
+      );
   }
 }
