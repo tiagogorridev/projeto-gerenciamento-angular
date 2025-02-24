e87d407b882cfdefad920979fb5bfea134bde298
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-24 11:50:27 -0300

    fix: corrigido método para obter detalhes da tarefa no TarefaService

--- src/app/core/auth/services/tarefa.service.ts
+++ src/app/core/auth/services/tarefa.service.ts
@@ -33,4 +33,31 @@ export class TarefaService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.delete<void>(`${this.apiUrl}/${tarefaId}`, { headers });
   }
+
+  getTarefaDetails(idprojeto: number, idtarefa: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/detalhes/${idprojeto}/${idtarefa}`).pipe(
+      tap(response => {
+        console.log('Resposta da API:', response);  // Verifique o formato da resposta
+      }),
+      catchError(error => {
+        console.error('Erro no serviço:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  atualizarTarefa(idprojeto: number, idtarefa: number, tarefa: Tarefa): Observable<any> {
+    const token = localStorage.getItem('auth_token') || '';
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put<any>(`${this.apiUrl}/detalhes/${idprojeto}/${idtarefa}`, tarefa, { headers }).pipe(
+      tap(response => {
+        console.log('Tarefa atualizada:', response);
+      }),
+      catchError(error => {
+        console.error('Erro ao atualizar tarefa:', error);
+        return throwError(error);
+      })
+    );
+  }
+
 }
