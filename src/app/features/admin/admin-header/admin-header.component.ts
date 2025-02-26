78e529d914bf351c2bf850c9bb840209a7cef16c
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-02-26 09:17:55 -0300

    fix: corrigir estrutura do componente AdminHeader e implementar funcionalidade de logout

--- src/app/features/admin/admin-header/admin-header.component.ts
+++ src/app/features/admin/admin-header/admin-header.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
   styleUrls: ['./admin-header.component.scss']
 })
-export class AdminHeaderComponent {
+export class AdminHeaderComponent implements OnInit {
+  showProfileMenu: boolean = false;
+  isAuthenticated$ = this.authService.isAuthenticated$;
 
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+
+  logout() {
+    this.authService.logout().subscribe();
+  }
+
+  onSearch(event: any): void {
+  }
+
+  toggleProfileMenu(): void {
+    this.showProfileMenu = !this.showProfileMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: any): void {
+    const profileElement = event.target.closest('.user-profile');
+    if (!profileElement && this.showProfileMenu) {
+      this.showProfileMenu = false;
+    }
+  }
 }
