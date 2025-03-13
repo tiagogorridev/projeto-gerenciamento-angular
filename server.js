e2090e6d58ddc1cc86d2e2c38a54afa62e4e4424
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-03-12 22:24:47 -0300

    Clean up configuration for Heroku deployment

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Get the correct output path from your angular.json
-const outputPath = 'dist/projeto-gerenciamento';  // Adjust this based on your angular.json
+const outputPath = 'dist/projeto-gerenciamento';
 
-// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, outputPath)));
 
-// Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, outputPath, 'index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 console.log('Server started on port ' + (process.env.PORT || 8080));
